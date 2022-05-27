09cd2c0a7b4dd0f52d31a5e6a1b1d25105c5ed60
yozadesu <19c1050015yk@edu.tech.ac.jp>
2022-05-27 18:24:21 +0900

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<section class="wrapper">
+    <h2 class="text-red-200 text-5xl">自己紹介</h2>
+    <ul>
+        <li>生年月日：<input type="date" name="calender"></li>
+        <li>名前：吉澤昂希</li>
+        <li>特技：⚽</li>
+        <li>趣味：🎤🎶</li>
+        <p>ここはページ１です</p>
+        <a href="/page2">page2</a>
+        <a href="/page3">page3</a>
+
+        /*classの作法的にはアルファベット順に書くのがベスト*/
+        
+        
+    </ul>
+</section>
---
+++ src/routes/page3.svelte
@@ -0,0 +1,2 @@
+<h1>page3</h1>
+<a href="/">homeに戻る</a>
